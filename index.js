749beeb0bccaf269f33d817cb1901b2634cecb7c
swadhinroutray <swadhin.routray@gmail.com>
2020-09-07 17:18:58 +0530

    Added a NLP type Model in JS with a flask API

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 const port = process.env.PORT || 8080;
 const axios = require("axios");
 const web = new WebClient(process.env.SLACK_TOKEN);
+const  qs = require('querystring')
 // const { createEventAdapter } = require('@slack/events-api');
 // const slackEvents = createEventAdapter(process.env.SLACK_SECRET);
 // app.use('/slack/events',slackEvents.expressMiddleware())
 const fetch = require("node-fetch");
 app.use(require("body-parser").json());
 app.use(require("body-parser").urlencoded({ extended: true }));
+
+const CLIMATE = ["weather", "temperature", "climate","Climate","Temperature","Weather"];
+
 app.post("/hello", async (req, res) => {
   try {
     await web.chat.postMessage({
@@ -20,37 +24,53 @@ app.post("/hello", async (req, res) => {
     return res.send(`Hello @${req.body.user_name}`);
   } catch (error) {
     console.log(error);
-    return res.send(error);
+    return res.json({ok:true});
   }
 });
 
-app.post("/getText", async (req, res) => {
-  console.log(req.body);
-
-  return res.send(req.body.text);
+app.post("/getText", (req, res) => {
+  // console.log(req.body);
+  if (req.body.event.bot_profile!==undefined &&req.body.event.bot_profile.id === 'B01A41E149K') { 
+    return res.end();
+  }else{
+  const event = req.body.event;
+  console.log(event)
+  let flag = 0;
+  for (i = 0; i < CLIMATE.length; i++) {
+    if (event.text.includes(CLIMATE[i])) {
+      flag = 1;
+      break;
+    }
+  }
+  if(flag==1){
+    console.log('checking')
+    Weather(req,res)
+    res.sendStatus(200)
+  }
+  else{
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end();
+  }
+}
 });
 
 //TODO: Return Weather with proper formatting and log as well
 app.post("/weather", async (req, res) => {
-  const city = req.body.text.trim();
 
-  await fetch(
-    process.env.SLACK_WEBHOOK_URL,
-    {
-      method: "POST",
-      body: {
-        text: `Let me check the weather in ${city} for you!`,
-        response_type: "ephemeral",
-      },
-    }
-  );
+  await fetch(process.env.SLACK_WEBHOOK_URL, {
+    method: "POST",
+    body: {
+      text: `Let me check the weather in ${city} for you!`,
+      response_type: "ephemeral",
+    },
+  });
   console.log(city);
   try {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`;
     fetch(url)
       .then((data) => data.json())
       .then(async (resp) => {
-        console.log(resp);
+        // console.log(resp);
         const tempA = Math.trunc(resp.main.temp - 273);
         const tempF = Math.trunc(resp.main.feels_like - 273);
         const humidity = resp.main.humidity;
@@ -68,13 +88,12 @@ app.post("/weather", async (req, res) => {
   }
 });
 
-app.post("/getCity", async (req, res) => {
-  const body = { text: req.body.text.trim() };
-  console.log(body);
+ async function Weather (req, res) {
+  // console.log(body);
   const response = await axios.post(
     "https://city-extractor.herokuapp.com/getcity",
     {
-      text: req.body.text.trim(),
+      text: req.body.event.text.trim(),
     },
     {
       headers: {
@@ -82,25 +101,28 @@ app.post("/getCity", async (req, res) => {
       },
     }
   );
+  let text;
+  let message={}
   console.log(response.data.city[0]);
-;
   const CITY = response.data.city[0];
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${process.env.WEATHER_API_KEY}`;
   fetch(url)
     .then((data) => data.json())
     .then(async (resp) => {
-      console.log(resp);
+      // console.log(resp);
       const tempA = Math.trunc(resp.main.temp - 273);
       const tempF = Math.trunc(resp.main.feels_like - 273);
       const humidity = resp.main.humidity;
-      const text = `Weather in ${CITY} is ${tempA}\xB0C but feels like ${tempF}\xB0C with ${humidity}% humidity`;
+      text = `${CITY} is at ${tempA}\xB0C but feels like ${tempF}\xB0C with ${humidity}% humidity`;
       console.log(text);
-
-      // console.log(text);
-      return res.send(text);
+      await web.chat.postMessage({
+        channel: process.env.CHANNEL,
+        text: text,
+      });
+    return res.end()
     });
   // res.send("completed");
-});
+};
 
 app.listen(port, (err) => {
   if (err) return res.send(err);
